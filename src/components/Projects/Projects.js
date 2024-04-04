59505f7d705cb3eab392c4a24b534030717ab798
Nouffel  M <nouffel_95400@hotmail.fr>
2024-04-04 02:53:45 +0200

    image tp7

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,6 +7,7 @@ import calculatrice from "../../Assets/Projects/calculatricec.png";
 import portfolio from "../../Assets/Projects/portfolio.png";
 import personnel from "../../Assets/Projects/personnel.png";
 import parking from "../../Assets/Projects/parking.png";
+import tp7 from "../../Assets/Projects/tp7.png";
 
 function Projects() {
   return (
@@ -67,7 +68,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              // imgPath={}
+              imgPath={tp7}
               title="Tp Seven"
               description="Projet d'accompagnement des utilisateurs pour windows7 "
               ghLink="https://github.com/MohamedMERHDAOUI/tp7doc/blob/main/tp-seven.pdf"
