c6d8b77d14df83dfdc9dd571b23979673222e0dc
Nouffel  M <nouffel_95400@hotmail.fr>
2024-04-04 02:56:27 +0200

    projets

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -90,7 +90,7 @@ function NavBar() {
                 <AiOutlineFundProjectionScreen
                   style={{ marginBottom: "2px" }}
                 />{" "}
-                Projet
+                Projets
               </Nav.Link>
             </Nav.Item>
 
