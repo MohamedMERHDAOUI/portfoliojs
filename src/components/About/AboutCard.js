f59e4000e7f3d2b8dbbd4865a8c04f21c69a05b8
Mounanou Alexandre <144010152+MohamedMERHDAOUI@users.noreply.github.com>
2025-05-21 22:08:10 +0200

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
           Bonjour je suis  <span className="blue">Alexandre Mounanou </span>
-          après l'obtention de mon baccalauréat général option physique - Chimie et sciences de l'ingénieur,  j'ai décidé de suivre une formation de développement à Epitech. Par la suite, j'ai intégré un BTS SIO (Services Informatiques aux Organisations) avec l'option SLAM (Solutions Logicielles et Applications Métiers) au sein de L'UFA Charles de Foucauld.
+          après l'obtention de mon baccalauréat général option physique - Chimie et SVT,  j'ai décidé d'intégré un BTS SIO (Services Informatiques aux Organisations) avec l'option SLAM (Solutions Logicielles et Applications Métiers) au sein de L'IRIS, École Supérieure de l'Informatique et du Numérique.
         
           </p>
         
