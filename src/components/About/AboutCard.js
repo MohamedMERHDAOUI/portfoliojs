c3be13b57c47780b8a42e68638c46ea2f4276c33
Mounanou Alexandre <144010152+MohamedMERHDAOUI@users.noreply.github.com>
2025-05-21 21:30:07 +0200

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,7 +7,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-          Bonjour je suis  <span className="blue">Merhdaoui Mohamed </span>
+          Bonjour je suis  <span className="blue">Alexandre Mounanou </span>
           après l'obtention de mon baccalauréat général option physique - Chimie et sciences de l'ingénieur,  j'ai décidé de suivre une formation de développement à Epitech. Par la suite, j'ai intégré un BTS SIO (Services Informatiques aux Organisations) avec l'option SLAM (Solutions Logicielles et Applications Métiers) au sein de L'UFA Charles de Foucauld.
         
           </p>
